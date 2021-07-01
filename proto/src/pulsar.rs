3beb8ecbb4d56a4a4f55881178a2167b08c23768
Rui Fu <freelz@gmail.com>
2021-07-01 13:22:54 +0800

    fix proto

--- proto/src/pulsar.rs
+++ proto/src/pulsar.rs
@@ -1,5 +1,9 @@
 //! Pulsar-proto auto-generated sub-modules for Pulsar
 
+pub mod proto {
+    include!("prost/proto.rs");
+}
+
 pub mod meta {
     pub const REPOSITORY: &str = "https://github.com/apache/pulsar";
     pub const COMMITISH: &str = "v2.8.0";
